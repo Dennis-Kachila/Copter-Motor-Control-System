ae2c21ee36da02bf0761a159af2495ba331078c8
Dennis Kachila <denvilla55@gmail.com>
2021-09-23 22:45:55 +0300

    first submission

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,40 @@
 #include <Arduino.h>
-int led = 13;
+const int positionValue = A5;
+const int motorspeedoutput = 10;
+const float requiredangle = 90.0;
 void setup() {
+  Serial.begin(9600);
   // put your setup code here, to run once:
-  
-  pinMode(led, OUTPUT);
+  pinMode(motorspeedoutput, OUTPUT);
 }
 
+float readPositionAngle(){
+  int sensorValue = analogRead(positionValue);
+  float angle = sensorValue * (270.0 / 1023.0);
+  return angle;
+
+
+}
+void feedbackErrorCorrect(){
+  float errorValue = requiredangle - readPositionAngle();
+  Serial.print("error value is: ");
+  Serial.println(errorValue);
+  int errorMapped = map(errorValue,0.0, 270, 0, 255);
+  Serial.print("Value to be mapped : ");
+  Serial.println(errorMapped);
+ analogWrite(motorspeedoutput, errorMapped);
+
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(led, HIGH);
-  delay(1000);
-  digitalWrite(led, LOW);
-  delay(2000);
-   
-
+   readPositionAngle();
+   if(readPositionAngle()<=90){
+     Serial.println("adjusting motor speed");
+     feedbackErrorCorrect();
+   }
+   else
+   {
+     Serial.println("Peak Overshoot,AUTOCORRECTING");
+   }
+delay(50);
 }
