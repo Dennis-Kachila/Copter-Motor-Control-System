2d61b8b5c8a9cc28346e99dd5cdc3fa46dea4c73
Dennis Kachila <denvilla55@gmail.com>
2021-09-26 20:13:40 +0300

    works but not as expected

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+int i;
 const int positionValue = A5;
 const int motorspeedoutput = 10;
 const float requiredangle = 90.0;
@@ -10,36 +11,52 @@ void setup() {
 
 float readPositionAngle(){
   int sensorValue = analogRead(positionValue);
-  float angle = sensorValue * (90.0 / 1023.0);
+  float angle = sensorValue * (270.0 / 1023.0);
   return angle;
 
 
 }
-void feedbackErrorCorrect(){
+/*void feedbackErrorCorrect(){
   float errorValue = requiredangle - readPositionAngle();
   Serial.print("error value is: ");
   Serial.println(errorValue);
-  int errorMapped = map(errorValue,0.0, 90.0, 0, 255);
+  if(errorValue)
+  int errorMapped = map(errorValue,0.0, 90.0, 255, 0);
   Serial.print("Value to be mapped : ");
   Serial.println(errorMapped);
   Serial.println();
  analogWrite(motorspeedoutput, errorMapped);
 
 }
+*/
 void loop() {
   // put your main code here, to run repeatedly:
    readPositionAngle();
    Serial.print("current angle is: ");
    Serial.println(readPositionAngle());
-   if(readPositionAngle()<=90){
-     Serial.println("adjusting motor speed");
+   
+   if(readPositionAngle()>2){
+     Serial.println("Adjusting motor speed.....");
      Serial.println();
-     feedbackErrorCorrect();
+     for ( i= 45; i<=155;i++){
+
+     delay(500);
+     Serial.print("VALUE TO MOTOR OUTPUT:");
+     Serial.println(i);
+     analogWrite(motorspeedoutput,i);
+     
+     if (readPositionAngle()  >=2 && readPositionAngle()<=6){
+       analogWrite(motorspeedoutput, 150);
+       Serial.println("Motor speed and angle maintained maintained");
+     }
+     //feedbackErrorCorrect();
+   }
    }
    else
    {
      Serial.println("Peak Overshoot,AUTOCORRECTING");
      Serial.println();
    }
-delay(1000);
+  
+//delay(1000);
 }
